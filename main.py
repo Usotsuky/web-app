6b5c648434f01a9017397403f394e0e75bb90061
Nurik <Nurik@gmail.com>
2024-10-13 22:54:59 +0500

    Change host to 127.0.0.1

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ async def read_model(model_name: ModelName):
 
 
 if "__main__" == __name__:
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=8000)
