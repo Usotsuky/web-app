adb4f92a916bb59988722ef9606ec5544501925d
Nurik <Nurik@gmail.com>
2024-10-13 22:54:29 +0500

    Add Config class and generation schemas

--- users/schemas.py
+++ users/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 from typing import Annotated
 from annotated_types import MinLen, MaxLen
 
@@ -7,6 +7,8 @@ class CreateUser(BaseModel):
     # username: str = Field(..., min_length=1, max_length=15) validation and metadata
     username: Annotated[str, MinLen(3), MaxLen(15)]
     email: EmailStr
+
+    # model_config = ConfigDict(from_attributes=True)
 
     class Config:
         schema_extra = {
