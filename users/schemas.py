5805c0ca6ec26293186ed4aa2fa8c0bfbd6af529
Nurik <Nurik@gmail.com>
2024-10-13 22:18:41 +0500

    Schema for creating user

--- users/schemas.py
+++ users/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr
 from typing import Annotated
 from annotated_types import MinLen, MaxLen
 
@@ -7,3 +7,11 @@ class CreateUser(BaseModel):
     # username: str = Field(..., min_length=1, max_length=15) validation and metadata
     username: Annotated[str, MinLen(3), MaxLen(15)]
     email: EmailStr
+
+    class Config:
+        schema_extra = {
+            "example":{
+                "username": "admin",
+                "email": "admin@gmail.com",
+            }
+        }
