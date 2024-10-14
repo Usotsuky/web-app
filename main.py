d7489364c7211dda5062f67d4eb27fac0b216dba
Nurik <Nurik@gmail.com>
2024-10-14 13:51:23 +0500

    Adding product router to main router

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, Path
 from enum import Enum
 from typing import Annotated
 import uvicorn
+
+from core.config import settings
 from users.views import router as users_router
 from contextlib import asynccontextmanager
 from core.db import Base, db_helper
+from api_v1 import router as router_v1
 
 
 class ModelName(str, Enum):
@@ -12,13 +15,16 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(users_router)
 
 
