80fb6a3b929bf0c15de3427097b971a3834a81b1
Nurik <Nurik@gmail.com>
2024-10-14 13:48:55 +0500

    Scoped session and just session for views

--- core/db/db_helper.py
+++ core/db/db_helper.py
@@ -1,5 +1,12 @@
+from asyncio import current_task
+
 from ..config import settings
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
 
 
 class DatabaseHelper:
@@ -9,6 +16,21 @@ class DatabaseHelper:
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
 
+    def get_scoped_session(self):
+        return async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
 
 db_helper = DatabaseHelper(
     settings.db_url,
