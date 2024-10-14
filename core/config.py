b9aa56f1f355b1d5eefd5eaf903d1cb3173bbc58
Nurik <Nurik@gmail.com>
2024-10-14 13:50:50 +0500

    Prefix variable for api v1

--- core/config.py
+++ core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
     db_url: str = "postgresql+asyncpg://nurik:nurik@localhost:5433/postgres"
 
 
