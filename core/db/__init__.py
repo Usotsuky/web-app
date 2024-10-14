6c52400cd97c878356e065cdd50c091ed7aad284
Nurik <Nurik@gmail.com>
2024-10-14 22:54:17 +0500

    Continue of changing name of Products table

--- core/db/__init__.py
+++ core/db/__init__.py
@@ -1,10 +1,10 @@
 __all__ = (
     "Base",
-    "Product",
+    "Products",
     "DatabaseHelper",
     "db_helper",
 )
 
 from .base import Base
 from .db_helper import db_helper, DatabaseHelper
-from .products import Product
+from .products import Products
